fef382d4ac8d6b40d108fec63cb2e85d7a3c9ce9
Karen Siu <k3tang@gmail.com>
2022-07-11 16:39:36 -0700

    finish phase 2

--- skeleton4/lib/p02_hashing.rb
+++ skeleton4/lib/p02_hashing.rb
@@ -7,8 +7,8 @@ class Array
 
   def hash
     new_str = 0
-    self.each do |ele|
-      new_str += ele.hash
+    self.each.with_index do |ele, idx|
+      new_str += ele.hash * (idx + 1) 
     end
   return new_str
   end
@@ -17,6 +17,11 @@ end
 
 class String
   def hash
+    newnum = 0 
+    self.each_char.with_index do |ele, idx|
+      newnum += ele.ord * (idx + 1)
+    end 
+    newnum
   end
 end
 
@@ -24,6 +29,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    newnum = 0
+    self.each do |k,v|
+      newnum +=(k.hash + v.hash)
+    end 
+    newnum 
   end
 end
